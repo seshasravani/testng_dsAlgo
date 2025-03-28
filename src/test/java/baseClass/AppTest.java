3e8d7e46f668024e16006142d32afceebba476e6
Sesha Sravani <sraavz.aish@gmail.com>
2025-03-28 18:43:43 -0400

    removed errors

--- src/test/java/baseClass/AppTest.java
+++ src/test/java/baseClass/AppTest.java
@@ -4,8 +4,6 @@ import static org.testng.AssertJUnit.assertTrue;
 import org.testng.annotations.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
-
 /**
  * Unit test for simple App.
  */
@@ -16,6 +14,6 @@ public class AppTest {
      */
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue(true);
+       
     }
 }
