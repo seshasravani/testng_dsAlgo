1dea1b491c3a17d9866f74fae835d5239faefc2c
Sesha Sravani <sraavz.aish@gmail.com>
2025-03-28 18:20:15 -0400

    created testng maven project

---
+++ src/test/java/baseClass/AppTest.java
@@ -0,0 +1,21 @@
+package baseClass;
+
+import static org.testng.AssertJUnit.assertTrue;
+import org.testng.annotations.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
