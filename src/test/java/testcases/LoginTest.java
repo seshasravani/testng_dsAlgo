ba9b515d86860723a3a8c600a6f31724be94cc04
learnersteen <learnersteen@gmail.com>
2025-04-04 11:56:23 -0400

    update Login Test and Test Data Provider
    update Login Test and Test Data Provider

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,32 +1,28 @@
 package testcases;
 
 import java.io.IOException;
-
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
 import baseClass.BaseClass;
 import pageObjectModel.HomePom;
 import pageObjectModel.IntroductionPagePom;
 import pageObjectModel.LoginPom;
 import pageObjectModel.RegisterPom;
+import utilities.TestDataProvider;
+
 
 public class LoginTest extends BaseClass {
 
-	public LoginPom login;
-	public HomePom hp;
-	public IntroductionPagePom ip;
-	public RegisterPom regPage;
+	private LoginPom login;
+	private HomePom hp;
+	private IntroductionPagePom ip;
+	private RegisterPom regPage;
 
 	@BeforeMethod
 	public void loginToDSPortal() {
-
 		login = new LoginPom();
 		hp = new HomePom();
 		ip = new IntroductionPagePom();
@@ -34,63 +30,177 @@ public class LoginTest extends BaseClass {
 
 		hp.ClickHomePageGetStartedButton();
 		ip.clickOnSignInlink();
-		// login.dsAlgoLogin();
 	}
 
 	@Test(priority = 6)
 	public void signInDSValidCredentials() {
 		login.dsAlgoLogin();
-
 	}
 
-	// Test method using the data provider
-	@Test(priority = 1, dataProvider = "alertMessagesForEmptyFields")
-	public void testLoginButtonWithEmptyCredentials(String expectedAlertMessage)
-			throws IOException, InterruptedException, OpenXML4JException {
+	@Test(priority = 1, dataProvider = "alertMessagesForEmptyFields", dataProviderClass = TestDataProvider.class)
+	  public void testLoginButtonWithEmptyCredentials(String expectedAlertMessage)
+	            throws IOException, InterruptedException, OpenXML4JException {
 
-		// Assuming the "login" method fills the form and clicks the login button.
-		login.enterLoginFormFields("login", 0); // Fill fields with data from the Excel file
-		login.clickloginBtn(); // Click login button
+	        login.enterLoginFormFields("login", 0);
+	        login.clickloginBtn();
 
-		// Switch to the alert or validation message and fetch it.
-		String actualAlertMsg = regPage.switchToElementAndGetValidationMessage();
-
-		// Validate that the actual alert message contains the expected error text
-		// (instead of an exact match)
-		Assert.assertTrue(actualAlertMsg.contains(expectedAlertMessage), "The error message did not match.");
-	}
-
-	// DataProvider method to supply the alert messages
-	@DataProvider(name = "alertMessagesForEmptyFields")
-	public Object[][] getAlertMessagesForEmptyFields() {
-		return new Object[][] { { "Please fill out this field." } // Common message when fields are empty
-
-		};
-	}
-
-	@Test(priority = 2, dataProvider = "alertMessagesForMissingUsername")
-	public void testLoginButtonWithMissingUsername(String expectedAlertMessage)
-			throws IOException, InterruptedException, OpenXML4JException {
-
-		// Assuming the "login" method fills the form and clicks the login button.
-		login.enterLoginFormFields("login", 0); // Fill fields with data from the Excel file
-		login.clickloginBtn(); // Click login button
-
-		// Switch to the alert or validation message and fetch it.
-		String actualAlertMsg = regPage.switchToElementAndGetValidationMessage();
-
-		// Validate that the actual alert message contains the expected error text
-		// (instead of an exact match)
-		Assert.assertTrue(actualAlertMsg.contains(expectedAlertMessage), "The error message did not match.");
-	}
-
-// DataProvider method to supply the alert messages
-	@DataProvider(name = "alertMessagesForMissingUsername")
-	public Object[][] getAlertMessagesForMissingUsernameField() {
-		return new Object[][] { { "Please fill out this field." } // Common message when fields are empty
-
-		};
-
-	}
-
+	        String actualAlertMsg = regPage.switchToElementAndGetValidationMessage();
+	        Assert.assertTrue(actualAlertMsg.contains(expectedAlertMessage),
+	                "Expected: " + expectedAlertMessage + ", but got: " + actualAlertMsg);
+	    }
+	
 }
+
+
+//
+//	@Test(priority = 2, dataProvider = "alertMessagesForMissingUsername", dataProviderClass = TestDataProvider.class)
+//	public void testLoginButtonWithMissingUsername(String expectedAlertMessage)
+//			throws IOException, InterruptedException, OpenXML4JException {
+//		login.enterLoginFormFields("login", 0);
+//		login.clickloginBtn();
+//
+//		String actualAlertMsg = regPage.switchToElementAndGetValidationMessage();
+//		Assert.assertTrue(actualAlertMsg.contains(expectedAlertMessage),
+//				"Expected: " + expectedAlertMessage + ", but got: " + actualAlertMsg);
+//	}
+//
+//	@Test(priority = 3, dataProvider = "loginTestData", dataProviderClass = DataProviderClass.class)
+//	public void validateLoginFunctionality(String username, String password, String expectedMessage)
+//			throws IOException, InterruptedException, OpenXML4JException {
+//		login.enterUsernameTxt(username);
+//		login.enterPasswordTxt(password);
+//		login.clickloginBtn();
+//
+//		String actualAlertMsg = regPage.switchToElementAndGetValidationMessage();
+//		Assert.assertTrue(actualAlertMsg.contains(expectedMessage),
+//				"Expected: " + expectedMessage + ", but got: " + actualAlertMsg);
+//	}
+//}
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package testcases;
+//
+//import java.io.IOException;
+//
+//import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
+//import org.testng.Assert;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.DataProvider;
+//import org.testng.annotations.Parameters;
+//import org.testng.annotations.Test;
+//
+//import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+//
+//import baseClass.BaseClass;
+//import pageObjectModel.HomePom;
+//import pageObjectModel.IntroductionPagePom;
+//import pageObjectModel.LoginPom;
+//import pageObjectModel.RegisterPom;
+//
+//public class LoginTest extends BaseClass {
+//
+//	public LoginPom login;
+//	public HomePom hp;
+//	public IntroductionPagePom ip;
+//	public RegisterPom regPage;
+//
+//	@BeforeMethod
+//	public void loginToDSPortal() {
+//
+//		login = new LoginPom();
+//		hp = new HomePom();
+//		ip = new IntroductionPagePom();
+//		regPage = new RegisterPom();
+//
+//		hp.ClickHomePageGetStartedButton();
+//		ip.clickOnSignInlink();
+//		// login.dsAlgoLogin();
+//	}
+//
+//	@Test(priority = 6)
+//	public void signInDSValidCredentials() {
+//		login.dsAlgoLogin();
+//
+//	}
+//
+//	// Test method using the data provider
+//	@Test(priority = 1, dataProvider = "alertMessagesForEmptyFields")
+//	public void testLoginButtonWithEmptyCredentials(String expectedAlertMessage)
+//			throws IOException, InterruptedException, OpenXML4JException {
+//
+//		// Assuming the "login" method fills the form and clicks the login button.
+//		login.enterLoginFormFields("login", 0); // Fill fields with data from the Excel file
+//		login.clickloginBtn(); // Click login button
+//
+//		// Switch to the alert or validation message and fetch it.
+//		String actualAlertMsg = regPage.switchToElementAndGetValidationMessage();
+//
+//		// Validate that the actual alert message contains the expected error text
+//		// (instead of an exact match)
+//		Assert.assertTrue(actualAlertMsg.contains(expectedAlertMessage), "The error message did not match.");
+//	}
+//
+//	// DataProvider method to supply the alert messages
+//	@DataProvider(name = "alertMessagesForEmptyFields")
+//	public Object[][] getAlertMessagesForEmptyFields() {
+//		return new Object[][] { { "Please fill out this field." } // Common message when fields are empty
+//
+//		};
+//	}
+//
+//	@Test(priority = 2, dataProvider = "alertMessagesForMissingUsername")
+//	public void testLoginButtonWithMissingUsername(String expectedAlertMessage)
+//			throws IOException, InterruptedException, OpenXML4JException {
+//
+//		// Assuming the "login" method fills the form and clicks the login button.
+//		login.enterLoginFormFields("login", 0); // Fill fields with data from the Excel file
+//		login.clickloginBtn(); // Click login button
+//
+//		// Switch to the alert or validation message and fetch it.
+//		String actualAlertMsg = regPage.switchToElementAndGetValidationMessage();
+//
+//		// Validate that the actual alert message contains the expected error text
+//		// (instead of an exact match)
+//		Assert.assertTrue(actualAlertMsg.contains(expectedAlertMessage), "The error message did not match.");
+//	}
+//
+//// DataProvider method to supply the alert messages
+//	@DataProvider(name = "alertMessagesForMissingUsername")
+//	public Object[][] getAlertMessagesForMissingUsernameField() {
+//		return new Object[][] { { "Please fill out this field." } // Common message when fields are empty
+//
+//		};
+//
+//	}
+//
+//}
--- src/test/java/utilities/TestDataProvider.java
+++ src/test/java/utilities/TestDataProvider.java
@@ -1,5 +1,46 @@
 package utilities;
 
-public class TestDataProvider {
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
-}
+import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
+import org.testng.annotations.DataProvider;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+public class TestDataProvider {
+	
+	private static final String EXCEL_PATH = "src/test/resources/Excel/TestData.xlsx";
+	ExcelReader excelReader = new ExcelReader();
+	
+	
+	// ********** LOGIN FEATURE DATA PROVIDER **********
+	
+	@DataProvider(name = "alertMessagesForEmptyFields")
+	public Object[][] getAlertMessagesForEmptyFields() {
+		return new Object[][] { { "Please fill out this field." } };
+	}
+	
+	
+		// ********** GENERIC METHOD TO READ EXCEL DATA **********
+	public void enterLoginFormFields(String sheetname, int row)
+			throws InvalidFormatException, IOException, OpenXML4JException, InterruptedException {
+		LoggerLoad.info("Inside enterLoginFormFields");
+
+		List<Map<String, String>> testdata = excelReader.readFromExcel("src/test/resources/Excel/TestData.xlsx", sheetname);
+		LoggerLoad.info("logintestdata");
+	
+		
+		String username = testdata.get(row).get("username");
+		enterUsernameTxt(username);
+		LoggerLoad.info("Fetched username from Excel: " + username);
+		
+		String password = testdata.get(row).get("password");
+		LoggerLoad.info("Fetched password from Excel: " + password);
+		enterPasswordTxt(password);
+	}	
+	}
+
+
+
