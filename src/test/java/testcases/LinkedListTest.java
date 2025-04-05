c96f8d2868ef7ba12893e2d174c248d342736d2c
learnersteen <learnersteen@gmail.com>
2025-04-04 22:58:58 -0400

    LinkedList updates
    LinkedList updates

--- src/test/java/testcases/LinkedListTest.java
+++ src/test/java/testcases/LinkedListTest.java
@@ -93,7 +93,8 @@ public class LinkedListTest extends BaseClass {
 		
 	}
 	
-			
+	//  #---------------------(Creating Linked LIst page)------------------------------
+	
 //		@Test(priority=5)
 //		public void testLLIntroTryBtnInvalidCode () {
 //			ip.clickDropdownToggle();
@@ -116,30 +117,336 @@ public class LinkedListTest extends BaseClass {
 //			LoggerLoad.info("Alert displayed");
 //		}
 	
+//	
+//	//@Test(retryAnalyzer = RetryAnalyzer.class, dataProvider = "llInValidcodeTopics", dataProviderClass = TestDataProvider.class)
+//	@Test(priority = 10, dataProvider = "invalidCodeData", dataProviderClass = TestDataProvider.class)
+//	public void testAlertLLIntroTryBtnInvalidCode () {
+//		ip.clickDropdownToggle();
+//		LoggerLoad.info("Dropdown Toggle clicked5");
+//		ip.clickDropdownItemLinkedList();
+//		LoggerLoad.info("Linked List Drop item selected5");
+//		ls.clicklsIntroductionLink();
+//		LoggerLoad.info("Introduction link clicked");
+//		tp.clickTryHereBtn();
+//		LoggerLoad.info("TryHere Button clicked");
+//		
+//		try {
+//		tp.enterTryHereCode("tryEditorCode");
+//			tp.clickRunButton();
+//		} catch (Exception e) {
+//			System.err.println("Error while entering code or clicking Run: " + e.getMessage());
+//		}
+//		Assert.assertEquals(true, tp.handleAlert());
+//		LoggerLoad.info("Alert displayed");
+//	}
 	
-	//@Test(retryAnalyzer = RetryAnalyzer.class, dataProvider = "llInValidcodeTopics", dataProviderClass = TestDataProvider.class)
-	@Test(priority = 5, dataProvider = "invalidCodeData", dataProviderClass = TestDataProvider.class)
-	public void testAlertLLIntroTryBtnInvalidCode () {
+	@Test(priority=5)
+	public void testClickCreateLLLink () {
 		ip.clickDropdownToggle();
-		LoggerLoad.info("Dropdown Toggle clicked5");
+		LoggerLoad.info("Dropdown Toggle clicked");
 		ip.clickDropdownItemLinkedList();
-		LoggerLoad.info("Linked List Drop item selected5");
-		ls.clicklsIntroductionLink();
-		LoggerLoad.info("Introduction link clicked");
+		LoggerLoad.info("Linked List Drop item selected");
+		ls.clicklsCreatingLinkedLIstLink();
+		LoggerLoad.info("Creating Linked List Link Clicked");
+		Assert.assertEquals("Creating Linked LIst", dsIntro.validatePageTitle());
+		LoggerLoad.info("Creating Linked List page opened");
+		
+	}
+	
+	
+	@Test(priority=6)
+	public void testCreateLLLinkTryBtnNoCode () {
+		ip.clickDropdownToggle();
+		LoggerLoad.info("Dropdown Toggle clicked");
+		ip.clickDropdownItemLinkedList();
+		LoggerLoad.info("Linked List Drop item selected");
+		ls.clicklsCreatingLinkedLIstLink();
+		LoggerLoad.info("Creating Linked List Link Clicked");
 		tp.clickTryHereBtn();
 		LoggerLoad.info("TryHere Button clicked");
+		tp.clickRunButton();
+		LoggerLoad.info("Run Button clicked");
+		Assert.assertEquals(false, tp.isAlertPresent());
+		LoggerLoad.info("No Alert  or error displayed");
 		
-		try {
-		tp.enterTryHereCode("tryEditorCode");
-			tp.clickRunButton();
-		} catch (Exception e) {
-			System.err.println("Error while entering code or clicking Run: " + e.getMessage());
-		}
-		Assert.assertEquals(true, tp.handleAlert());
-		LoggerLoad.info("Alert displayed");
 	}
+	
+	//  #---------------------------------(Types of Linked List page)-----------------------------------------
+	
+	@Test(priority=7)
+	public void testLLTypesofLLpage () {
+		ip.clickDropdownToggle();
+		LoggerLoad.info("Dropdown Toggle clicked");
+		ip.clickDropdownItemLinkedList();
+		LoggerLoad.info("Linked List Drop item selected");
+		ls.clicklsTypesOfLinkedListLink();
+		LoggerLoad.info("Types of Linked List page link clicked");
+		Assert.assertEquals("Types of Linked List", dsIntro.validatePageTitle());
+		LoggerLoad.info("Types of Linked List page displayed");
 		
+	}
 	
 	
-}
+	
+	@Test(priority=8)
+	public void testTypesofLLpageTryHerBtn () {
+		ip.clickDropdownToggle();
+		LoggerLoad.info("Dropdown Toggle clicked");
+		ip.clickDropdownItemLinkedList();
+		LoggerLoad.info("Linked List Drop item selected");
+		ls.clicklsTypesOfLinkedListLink();
+		LoggerLoad.info("Types of Linked List page link clicked");
+		tp.clickTryHereBtn();
+		LoggerLoad.info("TryHere Button clicked");
+		Assert.assertTrue(ip.validateElementDisplayed(tp.runButton));
+		LoggerLoad.info("Run Button displayed");
+	}
+
+	
+	@Test(priority=9)
+	public void testTypesofLLpageTryHerBtnNoCode () {
+		ip.clickDropdownToggle();
+		LoggerLoad.info("Dropdown Toggle clicked");
+		ip.clickDropdownItemLinkedList();
+		LoggerLoad.info("Linked List Drop item selected");
+		ls.clicklsTypesOfLinkedListLink();
+		LoggerLoad.info("Types of Linked List page link clicked");
+		tp.clickTryHereBtn();
+		LoggerLoad.info("TryHere Button clicked");
+		Assert.assertTrue(ip.validateElementDisplayed(tp.runButton));
+		LoggerLoad.info("Run Button displayed");
+		tp.clickRunButton();
+		LoggerLoad.info("Run Button clicked");
+		Assert.assertEquals(false, tp.isAlertPresent());
+		LoggerLoad.info("No Alert  or error displayed");
+	}
+//    #---------------------------------(Implement Linked List in Python)-----------------------------------------
+	
+	@Test(priority=10)
+	public void testImplementLLInPython () {
+		ip.clickDropdownToggle();
+		LoggerLoad.info("Dropdown Toggle clicked");
+		ip.clickDropdownItemLinkedList();
+		LoggerLoad.info("Linked List Drop item selected");
+	    ls.clicklsImplementLinkedListInPythonLink();
+	    LoggerLoad.info("Implement Linked List in Python link clicked");
+	    Assert.assertEquals("Implement Linked List in Python", dsIntro.validatePageTitle());
+		LoggerLoad.info("Implement Linked List in Python page displayed");
+		
+	}	
+	
+	
+	@Test(priority=11)
+	public void testImplementLLInPythonTryHerBtn () {
+		ip.clickDropdownToggle();
+		LoggerLoad.info("Dropdown Toggle clicked");
+		ip.clickDropdownItemLinkedList();
+		LoggerLoad.info("Linked List Drop item selected");
+	    ls.clicklsImplementLinkedListInPythonLink();
+	    LoggerLoad.info("Implement Linked List in Python link clicked");
+	    tp.clickTryHereBtn();
+		LoggerLoad.info("TryHere Button clicked");
+		Assert.assertTrue(ip.validateElementDisplayed(tp.runButton));
+		LoggerLoad.info("Run Button displayed");
+		
+	}	
+	
+	
+	@Test(priority=12)
+	public void testImplementLLInPythonTryHerBtnNoCode  () {
+		ip.clickDropdownToggle();
+		LoggerLoad.info("Dropdown Toggle clicked");
+		ip.clickDropdownItemLinkedList();
+		LoggerLoad.info("Linked List Drop item selected");
+	    ls.clicklsImplementLinkedListInPythonLink();
+	    LoggerLoad.info("Implement Linked List in Python link clicked");
+	    tp.clickTryHereBtn();
+		LoggerLoad.info("TryHere Button clicked");
+	    tp.clickRunButton();
+		LoggerLoad.info("Run Button clicked");
+		Assert.assertEquals(false, tp.isAlertPresent());
+		LoggerLoad.info("No Alert  or error displayed");
+		
+	}
+	
+//  #---------------------------------(Traversal)-----------------------------------------
+	
+	
+	@Test(priority=13)
+	public void testLLTraversalLink () {
+		ip.clickDropdownToggle();
+		LoggerLoad.info("Dropdown Toggle clicked");
+		ip.clickDropdownItemLinkedList();
+		LoggerLoad.info("Linked List Drop item selected");
+		 ls.clicklsTraversalLink();
+		 LoggerLoad.info("Traversal link clicked");
+		 Assert.assertEquals("Traversal", dsIntro.validatePageTitle());
+		 LoggerLoad.info("Traversal page displayed");
+		
+	}
+	
+	
+	@Test(priority=14)
+	public void testLLTraversalLinkTryHerBtn () {
+		 ip.clickDropdownToggle();
+		 LoggerLoad.info("Dropdown Toggle clicked");
+		 ip.clickDropdownItemLinkedList();
+		 LoggerLoad.info("Linked List Drop item selected");
+		 ls.clicklsTraversalLink();
+		 LoggerLoad.info("Traversal link clicked");
+		 tp.clickTryHereBtn();
+		 LoggerLoad.info("TryHere Button clicked");
+		 Assert.assertTrue(ip.validateElementDisplayed(tp.runButton));
+		 LoggerLoad.info("Run Button displayed");
+	}
+	
+	@Test(priority=15)
+	public void testLLTraversalLinkTryHerBtnNoCode () {
+		 ip.clickDropdownToggle();
+		 LoggerLoad.info("Dropdown Toggle clicked");
+		 ip.clickDropdownItemLinkedList();
+		 LoggerLoad.info("Linked List Drop item selected");
+		 ls.clicklsTraversalLink();
+		 LoggerLoad.info("Traversal link clicked");
+		  tp.clickTryHereBtn();
+			LoggerLoad.info("TryHere Button clicked");
+		    tp.clickRunButton();
+			LoggerLoad.info("Run Button clicked");
+			Assert.assertEquals(false, tp.isAlertPresent());
+			LoggerLoad.info("No Alert  or error displayed");
+	}
+	
+//  #---------------------------------(Insertion)-----------------------------------------
+	
+	@Test(priority=16)
+	public void testLLInsertionLink () {
+		ip.clickDropdownToggle();
+		LoggerLoad.info("Dropdown Toggle clicked");
+		ip.clickDropdownItemLinkedList();
+		LoggerLoad.info("Linked List Drop item selected");
+		 ls.clicklsInsertionLink();
+		 LoggerLoad.info("Insertion link clicked");
+		 Assert.assertEquals("Insertion", dsIntro.validatePageTitle());
+		 LoggerLoad.info("Insertion page displayed");
+		
+	}
+	
+	
+	@Test(priority=17)
+	public void testLLInsertionLinkTryHerBtn () {
+		 ip.clickDropdownToggle();
+		 LoggerLoad.info("Dropdown Toggle clicked");
+		 ip.clickDropdownItemLinkedList();
+		 LoggerLoad.info("Linked List Drop item selected");
+		 ls.clicklsInsertionLink();
+		 LoggerLoad.info("Insertion link clicked");
+		 tp.clickTryHereBtn();
+		 LoggerLoad.info("TryHere Button clicked");
+		 Assert.assertTrue(ip.validateElementDisplayed(tp.runButton));
+		 LoggerLoad.info("Run Button displayed");
+	}
+	
+	@Test(priority=18)
+	public void testLLInsertionLinkTryHerBtnNoCode () {
+		 ip.clickDropdownToggle();
+		 LoggerLoad.info("Dropdown Toggle clicked");
+		 ip.clickDropdownItemLinkedList();
+		 LoggerLoad.info("Linked List Drop item selected");
+		 ls.clicklsInsertionLink();
+		 LoggerLoad.info("Insertion link clicked");
+		  tp.clickTryHereBtn();
+			LoggerLoad.info("TryHere Button clicked");
+		    tp.clickRunButton();
+			LoggerLoad.info("Run Button clicked");
+			Assert.assertEquals(false, tp.isAlertPresent());
+			LoggerLoad.info("No Alert  or error displayed");
+	}
+	
+//  #---------------------------------(Deletion)-----------------------------------------
+
+	@Test(priority=16)
+	public void testLLDeletionLink () {
+		ip.clickDropdownToggle();
+		LoggerLoad.info("Dropdown Toggle clicked");
+		ip.clickDropdownItemLinkedList();
+		LoggerLoad.info("Linked List Drop item selected");
+		 ls.clicklsDeletionLink();
+		 LoggerLoad.info("Deletion link clicked");
+		 Assert.assertEquals("Deletion", dsIntro.validatePageTitle());
+		 LoggerLoad.info("Deletion page displayed");
+		
+	}
+	
+	
+	@Test(priority=17)
+	public void testLLDeletionLinkTryHereBtn () {
+		 ip.clickDropdownToggle();
+		 LoggerLoad.info("Dropdown Toggle clicked");
+		 ip.clickDropdownItemLinkedList();
+		 LoggerLoad.info("Linked List Drop item selected");
+		 ls.clicklsDeletionLink();
+		 LoggerLoad.info("Deletion link clicked");
+		 tp.clickTryHereBtn();
+		 LoggerLoad.info("TryHere Button clicked");
+		 Assert.assertTrue(ip.validateElementDisplayed(tp.runButton));
+		 LoggerLoad.info("Run Button displayed");
+	}
+	
+	@Test(priority=18)
+	public void testLLDeletionLinkTryHereBtnNoCode () {
+		 ip.clickDropdownToggle();
+		 LoggerLoad.info("Dropdown Toggle clicked");
+		 ip.clickDropdownItemLinkedList();
+		 LoggerLoad.info("Linked List Drop item selected");
+		 ls.clicklsDeletionLink();
+		 LoggerLoad.info("Deletion link clicked");
+		  tp.clickTryHereBtn();
+			LoggerLoad.info("TryHere Button clicked");
+		    tp.clickRunButton();
+			LoggerLoad.info("Run Button clicked");
+			Assert.assertEquals(false, tp.isAlertPresent());
+			LoggerLoad.info("No Alert  or error displayed");
+	}
+	
+	
+
+	@Test(priority=19)
+	public void testLLPracticeQnsLink () {
+		 ip.clickDropdownToggle();
+		 LoggerLoad.info("Dropdown Toggle clicked");
+		 ip.clickDropdownItemLinkedList();
+		 LoggerLoad.info("Linked List Drop item selected");
+		
+			
+	}
+	
+	
+	
+	
+	 @Test(priority = 120, dataProvider = "validCodeForTraversal", dataProviderClass = TestDataProvider.class)
+	    public void testRunValidCodeTraversalPage(String validCode) {
+	        ip.clickDropdownToggle();
+	        LoggerLoad.info("Dropdown Toggle clicked");
+	        ip.clickDropdownItemLinkedList();
+	        LoggerLoad.info("Linked List Drop item selected");
+	        ls.clicklsTraversalLink();
+	        LoggerLoad.info("Traversal link clicked");
+	        tp.clickTryHereBtn();
+	        LoggerLoad.info("TryHere Button clicked");
+
+	        try {
+	            tp.enterTryHereCode(validCode);
+	            tp.clickRunButton();
+	        } catch (Exception e) {
+	            System.err.println("Error while entering code or clicking Run: " + e.getMessage());
+	        }
+
+	        String consoleOutput = tp.getOutputText();
+			Assert.assertNotNull(consoleOutput, "Console output should not be null.");
+			LoggerLoad.info("Console output: " + consoleOutput);
+			Assert.assertNotNull(tp.getOutputText());
+	    }
+	}
+	
+
 
--- src/test/java/utilities/TestDataProvider.java
+++ src/test/java/utilities/TestDataProvider.java
@@ -9,20 +9,18 @@ import org.testng.annotations.DataProvider;
 
 public class TestDataProvider {
 	
-	//private static final String EXCEL_PATH = "src/test/resources/Excel/TestData.xlsx";
-	ExcelReader excelReader = new ExcelReader();
+	    private static final String EXCEL_PATH = "src/test/resources/Excel/TestData.xlsx";
+	    static ExcelReader excelReader = new ExcelReader();
+
 	
-	@DataProvider(name = "invalidCodeData")
-	public Object[][] getInvalidCodeData() throws InvalidFormatException, IOException {
-		System.out.println("Inside getInvalidCodeData() method");
-	    List<Map<String, String>> testData = excelReader.readFromExcel("src/test/resources/Excel/TestData.xlsx", "tryEditorCode");
-	    
-	    Object[][] data = new Object[testData.size()][1]; // Only one column (pCode)
+	    @DataProvider(name = "validCodeForTraversal")
+	    public Object[][] getValidCodeForTraversal() throws IOException {
+	        List<Map<String, String>> testData = excelReader.readFromExcel(EXCEL_PATH, "tryEditorCode");
+	        Object[][] data = new Object[testData.size()][1];  // 1 column for code
 
-	    for (int i = 0; i < testData.size(); i++) {
-	        data[i][0] = testData.get(i).get("pCode"); // Fetch code from "pCode" column
+	        for (int i = 0; i < testData.size(); i++) {
+	            data[i][0] = testData.get(i).get("validCode"); // Fetching code from Excel
+	        }
+	        return data;
 	    }
-
-	    return data;
 	}
-}
